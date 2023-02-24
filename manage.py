5713413605ef34134717e2fecbf7c7140dc0feb6
AdachiTetsuya <tetsuya_adachi@demia.co.jp>
2023-02-25 02:31:51 +0900

    .envを読み込む

--- manage.py
+++ manage.py
@@ -6,8 +6,9 @@ import sys
 
 def main():
     from dotenv import load_dotenv
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 
-    load_dotenv()
+    load_dotenv(dotenv_path)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tetsuya_site.settings.dev")
 
     from django.core.management import execute_from_command_line
