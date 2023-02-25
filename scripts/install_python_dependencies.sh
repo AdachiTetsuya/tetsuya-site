8e831679797fbcdd54d05a047a89719779cd7747
AdachiTetsuya <tetsuya_adachi@demia.co.jp>
2023-02-25 13:42:38 +0900

    python3.11を使用

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -2,7 +2,7 @@
 set -e
 cd /home/ubuntu/tetsuya-site
 sudo chown -R ubuntu:ubuntu /home/ubuntu/tetsuya-site
-python3 -m venv .venv
+python3.11 -m venv .venv
 . .venv/bin/activate
 pip install -U pip setuptools wheel --no-cache
 pip install -U -r requirements.txt --no-cache
